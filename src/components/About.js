c908bd811adab69126bd3f3ecbe03c5115631eb9
Evan Rosson <rossonevan7281@gmail.com>
2022-11-08 11:43:40 -0600

    Added info to About Me

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,14 @@
 
 function About () {
     return (
-        <div>
-            <h1>This is the About Me Page</h1>
+        <div className="text-white">
+            <h1>I am a full stack developer graduate from Flatiron School. Previously, I graduated from the University of Louisiana at Lafayette with a bachelor's degree in Mechanical Engineering.</h1>
+            <br></br>
+            <h1>In my free time, I am mostly on the computer. I have always had an interest in computers (how they worked, how they were put together). The most interesting things to me during my mechanical engineering degree were the 3D modeling design classes as well as the Arduino coding we created for a robotics class. I decided it was best to delve into the world of coding and learn the many facets of the tech world that I have always been interested in.</h1> 
+            <br></br>
+            <h1>At Flatiron School, I have learned Full Stack developer skills including JavaScript, JSX, HTML, CSS, React.js, Ruby and Ruby on Rails, Object-Oriented Programming (OOP). I learned the process of developing web applications from scratch and also working with a team of talented web developers.</h1>
+            <br></br>
+            <h1>In my career, I hope to utilize my skills learned from my mechanical engineering and software engineering experiences to assist a dynamic company with the best of my abilities to better help my employer and provide the best support for the customer.</h1>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@
 
 function Home () {
     return (
-        <div> 
-            HOME
+        <div className="text-white"> 
+            <h1>Hi my name is Evan Rosson. I am a Software Engineer specializing in web development and this is my Portfolio. I hope you enjoy my applications!</h1> 
         </div>
     )
 }
