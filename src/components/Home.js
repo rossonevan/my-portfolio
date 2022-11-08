0dee4aa046c45eefe769a01c6decb848e2d4239c
Evan Rosson <rossonevan7281@gmail.com>
2022-11-08 11:28:09 -0600

    Changed styling on Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@
 
 function Home () {
     return (
-        <div>
-            <p>Hi! My name is Evan Rosson and I am a Software Engineer specializing in web development.</p>
+        <div> 
+            HOME
         </div>
     )
 }
