f82e09594e23d8ca2ac3bb0f6b4da54331378ffb
Evan Rosson <rossonevan7281@gmail.com>
2022-12-06 12:31:51 -0600

    Added assests folder and videos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-
-
+import earth from '../assets/earth.mp4';
 
 function Home () {
     return (
-        <div className="text-white"> 
-            <h1>Hi my name is Evan Rosson. I am a Software Engineer specializing in web development and this is my Portfolio. I hope you enjoy my applications!</h1> 
+        <div>
+            {/* <div className='overlay'></div> */}
+            <video src={earth} autoPlay loop muted/>
+            <div className="text-white"> 
+                <h1>Hi my name is Evan Rosson. I am a Software Engineer specializing in web development and this is my Portfolio. I hope you enjoy my applications!</h1> 
+            </div>
         </div>
     )
 }
